05e0d92d3a749aa45ab694f8010258353b12dd0e
Thu Hà <thuharosy2003@gmail.com>
2022-07-17 22:10:40 +0700

    BTVN

--- buoi1/Exercise1.java
+++ buoi1/Exercise1.java
@@ -15,6 +15,8 @@ public class Exercise1 {
                     System.out.println("Bao Binh");
                 }else if(day > 0 && day < 20){
                     System.out.println("Ma Ket");
+                }else{
+                    System.out.println("Nhap ngay khong hop le!");
                 }
                 break;
             case 2:
@@ -23,12 +25,16 @@ public class Exercise1 {
                         System.out.println("Bao Binh");
                     }else if(day > 18 && day < 30){
                         System.out.println("Song Ngu");
+                    }else{
+                        System.out.println("Nhap ngay khong hop le!");
                     }
                 }else{
                     if (day > 0 && day < 19) {
                         System.out.println("Bao Binh");
                     }else if(day > 18 && day < 29){
                         System.out.println("Song Ngu");
+                    }else{
+                        System.out.println("Nhap ngay khong hop le!");
                     }
                 }
                 break;
@@ -37,6 +43,8 @@ public class Exercise1 {
                     System.out.println("Song Ngu");
                 }else if(day > 20 && day < 32){
                     System.out.println("Bach Duong");
+                }else{
+                    System.out.println("Nhap ngay khong hop le!");
                 }
                 break;
             case 4:
@@ -44,6 +52,8 @@ public class Exercise1 {
                     System.out.println("Bach Duong");
                 }else if(day > 20 && day < 31){
                     System.out.println("Kim Nguu");
+                }else{
+                    System.out.println("Nhap ngay khong hop le!");
                 }
                 break;
             case 5:
@@ -51,6 +61,8 @@ public class Exercise1 {
                     System.out.println("Kim Nguu");
                 }else if(day > 20 && day < 32){
                     System.out.println("Song Tu");
+                }else{
+                    System.out.println("Nhap ngay khong hop le!");
                 }
                 break;
             case 6:
@@ -58,6 +70,8 @@ public class Exercise1 {
                     System.out.println("Song tu");
                 }else if(day > 21 && day < 31){
                     System.out.println("Cu Giai");
+                }else{
+                    System.out.println("Nhap ngay khong hop le!");
                 }
                 break;
             case 7:
@@ -65,6 +79,8 @@ public class Exercise1 {
                     System.out.println("Cu Giai");
                 }else if(day >22 && day < 32){
                     System.out.println("Su Tu");
+                }else{
+                    System.out.println("Nhap ngay khong hop le!");
                 }
                 break;
             case 8:
@@ -72,6 +88,8 @@ public class Exercise1 {
                     System.out.println("Su Tu");
                 }else if(day > 22 && day < 32){
                     System.out.println("Xu Nu");
+                }else{
+                    System.out.println("Nhap ngay khong hop le!");
                 }
                 break;
             case 9:
@@ -79,6 +97,8 @@ public class Exercise1 {
                     System.out.println("Xu Nu");
                 }else if(day > 23 && day < 31){
                     System.out.println("Thien Binh");
+                }else{
+                    System.out.println("Nhap ngay khong hop le!");
                 }
                 break;
             case 10:
@@ -86,6 +106,8 @@ public class Exercise1 {
                     System.out.println("Thien Binh");
                 }else if(day > 24 && day < 32){
                     System.out.println("Bo Cap");
+                }else{
+                    System.out.println("Nhap ngay khong hop le!");
                 }
                 break;
             case 11:
@@ -93,6 +115,8 @@ public class Exercise1 {
                     System.out.println("Bo Cap");
                 }else if(day > 23 && day < 31){
                     System.out.println("Nhan Ma");
+                }else{
+                    System.out.println("Nhap ngay khong hop le!");
                 }
                 break;
             case 12:
@@ -100,6 +124,8 @@ public class Exercise1 {
                     System.out.println("Nhan Ma");
                 }else if(day > 22 && day < 32){
                     System.out.println("Ma Ket");
+                }else{
+                    System.out.println("Nhap ngay khong hop le!");
                 }
                 break;
             default:
